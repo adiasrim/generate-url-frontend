e8d968ced3a086a4ef0297c4c36abae8eb77f0d5
MirikAkhmedov <akhmedovmirik@gmail.com>
2021-11-18 18:40:20 +0500

    init;

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,82 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div id="app">
+        <!-- component -->
+        <div>
+            <div class="relative min-h-screen  grid bg-gray-800 ">
+                <div
+                    class="flex flex-col sm:flex-row items-center md:items-start sm:justify-center md:justify-start flex-auto min-w-0 ">
+                    <div
+                        class="relative sm:w-1/2 xl:w-3/5 bg-blue-500 h-full hidden md:flex flex-auto items-center justify-center p-10 overflow-hidden  text-white bg-no-repeat bg-cover relative"
+                        style="background-image: url(https://images.unsplash.com/photo-1637102839358-51288f0a30d8?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2070&q=80);">
+                        <div class="absolute bg-black  opacity-25 inset-0 z-0"></div>
+                        <div class="w-full  lg:max-w-2xl md:max-w-md z-10 items-center text-center ">
+                            <div class=" font-bold leading-tight mb-6 mx-auto w-full content-center items-center ">
+
+                            </div>
+                        </div>
+                    </div>
+
+                    <div
+                        class="md:flex md:items-center md:justify-left w-full sm:w-auto md:h-full xl:w-1/2 p-8  md:p-10 lg:p-14 sm:rounded-lg md:rounded-none ">
+                        <div class="max-w-xl w-full space-y-12">
+                            <div class="lg:text-left text-center">
+
+                                <div class="flex items-center justify-center ">
+                                    <div
+                                        class="bg-black flex flex-col w-80 border border-gray-900 rounded-lg px-8 py-10">
+
+                                        <form
+                                            @submit.prevent="addNewTodo"
+                                            class="flex flex-col space-y-8 mt-10">
+                                            <label class="font-bold text-lg text-white ">Add your link</label>
+                                            <input
+                                                type="text"
+                                                placeholder="your link"
+                                                class="border rounded-lg py-3 px-3 mt-4 bg-black border-indigo-600 placeholder-white-500 text-white"
+                                                v-model="full"
+                                            >
+                                            <button
+                                                class="border border-indigo-600 bg-black text-white rounded-lg py-3 font-semibold"
+                                                v-text=" 'Generate new link' "
+                                            />
+
+                                            <a
+                                                class="text-center  border border-indigo-600 bg-black text-white rounded-lg py-3 font-semibold"
+                                                target="_blank" :href="short">{{ short }}</a>
+                                        </form>
+                                    </div>
+                                </div>
+
+                            </div>
+
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
-    <router-view/>
-  </div>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+export default {
+    data() {
+        return {
+            short: 'your link',
+            full: null
+        }
+    },
 
-#nav {
-  padding: 30px;
+    methods: {
+        addNewTodo() {
+            axios.get('http://127.0.0.1:8000/api/addLink?link=' + this.full)
+                .then(response => {
+                    this.short = response.data.link
+                })
+        }
+    },
+    mounted() {
+        this.addNewTodo();
+    }
 }
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+</script>
--- src/main.js
+++ src/main.js
@@ -2,9 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 
+window.axios = require('axios');
 Vue.config.productionTip = false
 
 new Vue({
-  router,
-  render: function (h) { return h(App) }
+    router,
+    render: function (h) {
+        return h(App)
+    }
 }).$mount('#app')
